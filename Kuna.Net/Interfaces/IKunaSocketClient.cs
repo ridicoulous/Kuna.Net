f6b33a35e61421d376085e589d22d7ff096aeeac
ridicoulous <artemkuryanov@gmail.com>
2019-05-31 01:51:19 +0300

    up

--- Kuna.Net/Interfaces/IKunaSocketClient.cs
+++ Kuna.Net/Interfaces/IKunaSocketClient.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace Kuna.Net.Interfaces
 {
-    public interface IKunaSocketClient
+    public interface IKunaSocketClient:IDisposable
     {
         void SubscribeToOrderBookSideUpdates(string market, Action<KunaOrderBookUpdateEvent, string> onUpdate);
         void SubscribeToTrades(string market, Action<KunaTradeEvent,string> onUpdate);
--- Kuna.Net/KunaSocketClient.cs
+++ Kuna.Net/KunaSocketClient.cs
@@ -1,7 +1,9 @@
 ﻿using CryptoExchange.Net;
+using CryptoExchange.Net.Objects;
 using CryptoExchange.Net.Sockets;
 using Kuna.Net.Interfaces;
 using Kuna.Net.Objects;
+using Newtonsoft.Json.Linq;
 using PusherClient;
 using System;
 using System.Collections.Generic;
@@ -17,7 +19,36 @@ namespace Kuna.Net
         {
             _pusherClient = new Pusher("4b6a8b2c758be4e58868", new PusherOptions() { Encrypted = true, Endpoint = "pusher.kuna.io", ProtocolNumber = 7, Version = "3.0.0" });
             _pusherClient.Connect();
-            
+            _pusherClient.Error += _pusherClient_Error;
+            _pusherClient.ConnectionStateChanged += _pusherClient_ConnectionStateChanged;
+        }
+
+        private void _pusherClient_ConnectionStateChanged(object sender, ConnectionState state)
+        {
+            log.Write(CryptoExchange.Net.Logging.LogVerbosity.Debug, $"Pusher state is {state.ToString()}");
+
+            switch (state)
+            {
+                case ConnectionState.Initialized:
+                    break;
+                case ConnectionState.Connecting:                 
+                    break;
+                case ConnectionState.Connected:
+                    break;
+                case ConnectionState.Disconnected:
+                    break;
+                case ConnectionState.WaitingToReconnect:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void _pusherClient_Error(object sender, PusherException error)
+        {
+            log.Write(CryptoExchange.Net.Logging.LogVerbosity.Error, error.ToString());
+
         }
         #region Channels
         /// <summary>
@@ -51,16 +82,43 @@ namespace Kuna.Net
         }
         public override Task UnsubscribeAll()
         {
-            _pusherClient.Disconnect();
+            if(_pusherClient!=null)
+                _pusherClient.Disconnect();
             return Task.CompletedTask;
         }
         public override void Dispose()
-        {               
+        {            
             base.Dispose();            
         }
-        protected override bool SocketReconnect(SocketSubscription subscription, TimeSpan disconnectedTime)
+
+        protected override bool HandleQueryResponse<T>(SocketConnection s, object request, JToken data, out CallResult<T> callResult)
         {
-            return false;
+            throw new NotImplementedException();
+        }
+
+        protected override bool HandleSubscriptionResponse(SocketConnection s, SocketSubscription subscription, object request, JToken message, out CallResult<object> callResult)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool MessageMatchesHandler(JToken message, object request)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool MessageMatchesHandler(JToken message, string identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task<CallResult<bool>> AuthenticateSocket(SocketConnection s)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task<bool> Unsubscribe(SocketConnection connection, SocketSubscription s)
+        {
+            throw new NotImplementedException();
         }
     }
 }
