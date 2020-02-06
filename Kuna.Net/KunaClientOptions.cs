4f8de87d9f4f53c2f11c86bb725b8a1f8e392f31
Артем Курьянов <artemkuryanov@gmail.com>
2020-02-06 16:57:03 +0200

    added parameter for options, veriosn bump

--- Kuna.Net/KunaClientOptions.cs
+++ Kuna.Net/KunaClientOptions.cs
@@ -5,9 +5,9 @@ namespace Kuna.Net
 {
     public class KunaClientOptions : RestClientOptions
     {
-        public KunaClientOptions(string baseAddress= "https://kuna.io/api/v2") :base(baseAddress)
+        public KunaClientOptions(string baseAddress= "https://kuna.io/api/v2", TimeSpan? timeout=null) :base(baseAddress)
         {
-            this.RequestTimeout = TimeSpan.FromMilliseconds(1500);
+            this.RequestTimeout = timeout?? TimeSpan.FromMilliseconds(2700);
         }
     }
 }
