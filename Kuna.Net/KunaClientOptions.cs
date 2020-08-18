87853bb6779072b5318fd450470bec805d9fcf5d
ridicoulous <artemkuryanov@gmail.com>
2020-08-18 20:42:28 +0000

    init updating to shared http client

--- Kuna.Net/KunaClientOptions.cs
+++ Kuna.Net/KunaClientOptions.cs
@@ -1,13 +1,19 @@
 ﻿using CryptoExchange.Net.Objects;
 using System;
+using System.Net.Http;
 
 namespace Kuna.Net
 {
     public class KunaClientOptions : RestClientOptions
     {
+
+        public KunaClientOptions(HttpClient httpClient):base("https://kuna.io/api/v2")
+        {
+
+        }
         public KunaClientOptions(string baseAddress= "https://kuna.io/api/v2", TimeSpan? timeout=null) :base(baseAddress)
         {
-            this.RequestTimeout = timeout?? TimeSpan.FromMilliseconds(2700);
+            this.RequestTimeout = timeout?? TimeSpan.FromMilliseconds(3000);
         }
     }
 }
