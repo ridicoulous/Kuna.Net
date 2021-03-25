6919847c5f6c4627b6ec8430a22e6e8588a83442
ridicoulous <artemkuryanov@gmail.com>
2021-03-25 23:26:29 +0200

    new version implemenation

--- Kuna.Net.Tests/IntegrationTests.cs
+++ Kuna.Net.Tests/IntegrationTests.cs
@@ -8,43 +8,26 @@ using CryptoExchange.Net.Logging;
 
 namespace Kuna.Net.Tests
 {
-    public class UnitTest1
-    {        
+    public class IntegrationTests
+    {
+        KunaClient client;
+        public IntegrationTests()
+        {
+            client = GetClientWithAuthentication(false);
 
-        //CallResult<KunaPlacedOrder> PlaceOrder(OrderType type, OrderSide side, decimal volume, decimal price, string market);
-
-        //CallResult<KunaPlacedOrder> CancelOrder(long orderId);
-
-        //CallResult<List<KunaPlacedOrder>> GetMyOrders(string market, OrderState orderState = OrderState.Wait, int page = 1, string sort = "desc");
-
-        //CallResult<KunaPlacedOrder> GetOrderInfo(long orderId);
-
-        //CallResult<List<KunaTrade>> GetMyTrades(string market, DateTime? fromDate = null, long? fromId = null, long? toId = null, int limit = 1000, string sort = "asc");
-
-        //CallResult<List<KunaTraidingPair>> GetExchangeCurrenciesInfo();
-        //Task<CallResult<List<KunaOhclv>>> GetCandlesHistoryAsync(string symbol, int resolution, DateTime from, DateTime to, CancellationToken token = default);
-        IKunaClientV2 client = new KunaClient();
-        // [Fact(DisplayName = "ServerTime")]
-        // public void ShouldGetServerTime()
-        // {
-        //     var serverTime = client.GetServerTimeV2();
-        //     Assert.True(Math.Abs(serverTime.Data.Subtract( DateTime.UtcNow).Seconds)<2);
-        //     var c = client.GetCurrenciesV2();
-        //     Assert.True(c);
-        // }
+        }
         [Fact(DisplayName = "PlaceORder")]
         public void PlaceOrder()
         {
-            var c = GetClientWithAuthentication();
-            var book = c.GetOrderBook("btcusdt");
-            var o = c.PlaceOrder("btcusdt", Objects.V3.KunaOrderSide.Buy, Objects.V3.KunaOrderType.Limit, 1, 1);
-            if(o)
-            {
-                var cancel = c.CancelOrder(o.Data.Id);
-                var order = c.GetOrders(Objects.V3.KunaOrderStatus.Done);
-                Assert.True(order);
+            var o = client.PlaceOrder("btcusdt", Objects.V3.KunaOrderSide.Buy, Objects.V3.KunaOrderType.Limit, 1, 1);
+            if (o)
+            {               
+                var orders = client.GetOrders(Objects.V3.KunaOrderStatus.Wait);
+                var placed = client.GetOrder(o.Data.Id);
+                var cancel = client.CancelOrder(o.Data.Id);
+                Assert.True(orders);
             }
-          
+
         }
         [Fact(DisplayName = "GetMarketInfo")]
         public void ShouldGetMarketInfo()
@@ -57,7 +40,7 @@ namespace Kuna.Net.Tests
         {
             var orderbook = client.GetOrderBookV2("btcusdt");
             Assert.True(orderbook);
-            
+
             orderbook.Data.Asks.ShouldNotBeNull();
             orderbook.Data.Bids.ShouldNotBeNull();
         }
@@ -74,8 +57,7 @@ namespace Kuna.Net.Tests
 
         public void ShouldGetAccountInfo()
         {
-            var _client = GetClientWithAuthentication();
-            var accountData = _client.GetAccountInfoV2();
+            var accountData = client.GetBalances();
             Assert.True(accountData);
             accountData.Data.ShouldNotBeNull();
         }
@@ -94,7 +76,7 @@ namespace Kuna.Net.Tests
             history.Error.ShouldBeNull();
         }
 
-        private KunaClient GetClientWithAuthentication()
+        private KunaClient GetClientWithAuthentication(bool pro)
         {
             var config = new ConfigurationBuilder().AddJsonFile("keys.json").Build();
             var key = config["key"];
@@ -103,10 +85,11 @@ namespace Kuna.Net.Tests
             {
                 ApiCredentials = new CryptoExchange.Net.Authentication.ApiCredentials(key, secret),
                 LogVerbosity = CryptoExchange.Net.Logging.LogVerbosity.Debug,
-                LogWriters = new System.Collections.Generic.List<System.IO.TextWriter>() { new DebugTextWriter(), new ThreadSafeFileWriter("debug-client.log") }
+                LogWriters = new System.Collections.Generic.List<System.IO.TextWriter>() { new DebugTextWriter(), new ThreadSafeFileWriter("debug-client.log") },
+                IsProAccount = pro
 
-            }) ;
-            return client;            
+            });
+            return client;
         }
     }
 }
