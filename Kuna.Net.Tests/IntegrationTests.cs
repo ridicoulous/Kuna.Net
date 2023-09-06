b3863a9097b44ca35828d6bf2dcb13642aa21110
alokym86 <alokym86@gmail.com>
2023-09-06 17:22:32 +0300

    tests

--- Kuna.Net.Tests/IntegrationTests.cs
+++ Kuna.Net.Tests/IntegrationTests.cs
@@ -1,124 +1,255 @@
-using System;
-using Xunit;
-using Shouldly;
-using Microsoft.Extensions.Configuration;
 using System.Threading.Tasks;
-using CryptoExchange.Net.Logging;
-using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Kuna.Net;
+using Kuna.Net.Objects.V4;
+using Kuna.Net.Objects.V4.Requests;
 using Microsoft.Extensions.Logging;
-using CryptoExchange.Net.Authentication;
+using Xunit;
 
 namespace Kuna.Net.Tests
 {
-    public class IntegrationTests
+    [TestCaseOrderer(
+    ordererTypeName: "Kuna.Net.Tests.PriorityOrderer",
+    ordererAssemblyName: "Kuna.Net.Tests")]
+    public class KunaV4ApiClientIntegrationTests
     {
-        KunaClient client;
-        public IntegrationTests()
+        private readonly KunaV4ApiClient _apiClient;
+        private static readonly Guid ordId = Guid.NewGuid();
+        public KunaV4ApiClientIntegrationTests()
         {
-            client = GetClientWithAuthentication(false);
-
-        }
-        [Fact(DisplayName = "PlaceOrder")]
-        public  async Task PlaceOrder()
-        {
-            var order = await client.ClientV3.PlaceOrderAsync("btcusdt", Objects.V3.KunaOrderSide.Sell, Objects.V3.KunaOrderType.Limit, 0.00001m, 188888m);
-
-            Assert.True(order);
-
-            var cancel = await client.CommonSpotClient.CancelOrderAsync(order.Data.Id.ToString());
-            Assert.True(cancel);
-
-        }
-        [Fact(DisplayName = "Bulk")]
-
-        public async Task BulkTest()
-        {
-            var book = await client.CommonSpotClient.GetOrderBookAsync("btcusdt");
-            Assert.True(book);
-            var activeOrders = await client.ClientV3.GetActiveOrdersAsync();
-            Assert.True(activeOrders);
-            //var closed = await client.ClientV3.GetClosedOrdersAsync();
-            //Assert.True(closed);
-            //var trades = await client.ClientV3.GetOrdersWithTradesAsync("btcusdt");
-            //Assert.True(trades);
-            var recentTrades = await client.ClientV3.GetRecentPublicTradesAsync("btcusdt");
-            Assert.True(recentTrades);
-            
-
-        }
-
-        [Fact(DisplayName = "GetMarketInfo")]
-        public void ShouldGetMarketInfo()
-        {
-            var marketInfo = client.ClientV2.GetMarketInfoV2("btcusdt");
-            Assert.True(marketInfo);
-        }
-        [Fact(DisplayName = "OrderBook")]
-        public void SholdGetOrderbook()
-        {
-            var orderbook = client.ClientV2.GetOrderBookV2("btcusdt");
-            Assert.True(orderbook);
-
-            orderbook.Data.Asks.ShouldNotBeNull();
-            orderbook.Data.Bids.ShouldNotBeNull();
-        }
-        [Fact(DisplayName = "TradesHistory")]
-        public void ShoulGetTradesHistory()
-        {
-            var trades = client.ClientV2.GetTradesV2("btcusdt");
-            Assert.True(trades);
-            trades.Data.ShouldNotBeNull();
-            trades.Error.ShouldBeNull();
-        }
-
-        [Fact(DisplayName = "Account")]
-
-        public void ShouldGetAccountInfo()
-        {
-            var accountData = client.ClientV3.GetBalances();
-            Assert.True(accountData);
-            accountData.Data.ShouldNotBeNull();
-        }
-
-        [Fact(DisplayName = "CandlesHistoryAsync")]
-        public async Task ShouldGetCandlesHistoryAsync()
-        {
-            var date = new DateTime(2020, 01, 01);
-            var date1 = date.AddDays(-1);
-            var date2 = date.AddDays(0);
-            var history = await client.ClientV3.GetCandlesHistoryV2Async("btcusd", 60, date1, date2);
-
-            Assert.True(history);
-            history.Data.ShouldNotBeNull();
-            //history.Data.Count.ShouldBe(25);
-            //history.Error.ShouldBeNull();
-        }
-
-        private KunaClient GetClientWithAuthentication(bool pro)
-        {
-            var config = new ConfigurationBuilder().AddJsonFile("keys.json").Build();
+            var config = new ConfigurationBuilder().AddJsonFile("keys.json", optional: true).Build();
             var key = config["key"];
             var secret = config["secret"];
-            
+            var singleKey = config["sinle-api-key"];
 
-            ApiCredentials c = string.IsNullOrEmpty(key) ? null : new CryptoExchange.Net.Authentication.ApiCredentials(key, secret);
-            var client = new KunaClient(new KunaClientOptions()
+            KunaApiCredentials? cred = null;
+            if (!string.IsNullOrEmpty(singleKey))
             {
-                ApiCredentials = c,
+                cred = new KunaApiCredentials(singleKey);
+            }
+            if (!string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(secret))
+            {
+                cred = new KunaApiCredentials(key, secret);
+            }
+            _apiClient = (KunaV4ApiClient)new KunaClient(
+                new KunaClientOptions()
+                {
+                    LogLevel = LogLevel.Trace,
+                    ApiCredentials = cred
+                }).ClientV4;
+        }
 
-                LogLevel = LogLevel.Debug,
-                
-                LogWriters = new List<ILogger>() { new DebugLogger() , new ConsoleLogger() },
-                IsProAccount = pro,
-                //RateLimiters = new List<CryptoExchange.Net.Interfaces.IRateLimiter>() { new RateLimiterTotal(1100, TimeSpan.FromMinutes(1)) },
-                //RateLimitingBehaviour = CryptoExchange.Net.Objects.RateLimitingBehaviour.Fail,
-                RequestTimeout = TimeSpan.FromSeconds(4)
+
+        [Fact]
+        public async Task GetServerTimeAsyncTest()
+        {
+            // Arrange
+            var today = DateTime.Now.Date;
+            // Act
+            var result = await _apiClient.GetServerTimeAsync();
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Equal(today, result.Data.Date);
+        }
 
 
-            });
-            
-            return client;
-            
+        [Fact]
+        public async Task GetTickersAsyncTest()
+        {
+            // Arrange
+
+            // Act
+            var result = await _apiClient.GetTickersAsync();
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.IsType<KunaTickerV4>(result.Data.First());
+        }
+        [Fact]
+        public async Task GetTickersAsyncForBTCUAH_BTCSUDT_Test()
+        {
+            // Arrange
+            var symbol1 = "BTC_UAH";
+            var symbol2 = "BTC_USDT";
+            // Act
+            var result = await _apiClient.GetTickersAsync(default, symbol1, symbol2);
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Contains(result.Data, t => t.Pair == symbol1);
+            Assert.Contains(result.Data, t => t.Pair == symbol2);
+        }
+        [Fact]
+        public async Task GetCurrenciesAsyncTest()
+        {
+            // Arrange
+
+            // Act
+            var result = await _apiClient.GetCurrenciesAsync();
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Contains(result.Data, c => c.Type == CurrencyType.Crypto);
+            Assert.Contains(result.Data, c => c.Type == CurrencyType.Fiat);
+
+        }
+        [Fact]
+        public async Task GetCurrenciesAsyncOnlyCryptoTest()
+        {
+            // Arrange
+
+            // Act
+            var result = await _apiClient.GetCurrenciesAsync(CurrencyType.Crypto);
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Contains(result.Data, c => c.Type == CurrencyType.Crypto);
+            Assert.DoesNotContain(result.Data, c => c.Type == CurrencyType.Fiat);
+
+        }
+        [Fact]
+        public async Task GetRecentPublicTradesAsyncTest()
+        {
+            // Arrange
+            var pair = "BTC_UAH";
+            var today = DateTime.Now.Date;
+            // Act
+            var result = await _apiClient.GetRecentPublicTradesAsync(pair);
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Contains(result.Data, c => c.Pair == pair);
+            Assert.Contains(result.Data, c => c.CreatedAt.Date == today);
+
+        }
+
+        [Fact]
+        public async Task GetTradingPairsAsyncTest()
+        {
+            // Arrange
+            var pair = "BTC_UAH";
+            // Act
+            var result = await _apiClient.GetTradingPairsAsync();
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Contains(result.Data, c => c.Pair == pair);
+        }
+
+        [Fact]
+        public async Task GetOrderBookAsyncTest()
+        {
+            // Arrange
+            var pair = "BTC_UAH";
+            // Act
+            var result = await _apiClient.GetOrderBookAsync(pair, OrderBookLevel.Five);
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.True(result.Data.Asks.First().Price > result.Data.Bids.First().Price);
+        }
+
+        [Fact]
+        public async Task SendAsProTest()
+        {
+            // Arrange
+            var pair = "BTC_UAH";
+
+            // Act
+            _apiClient.SetProAccount(true);
+            var result = await _apiClient.GetOrderBookAsync(pair, OrderBookLevel.Twenty);
+            _apiClient.SetProAccount(false);
+            var proHeaderEntry = result.RequestHeaders?.FirstOrDefault(kv => kv.Key == "account");
+            // Assert
+            Assert.True(result.Success);
+
+            // Add more assertions based on the expected behavior of this method
+            Assert.Contains(proHeaderEntry.Value.Value, p => p.Equals("pro"));
+        }
+
+        [Fact, TestPriority(1)]
+        public async Task PlaceOrderAsyncTest()
+        {
+            // Arrange
+            var symbol = "BTC_UAH";
+            var price = 10m;
+            var amount = 0.1m;
+            var side = KunaOrderSideV4.Bid;
+            var req = PlaceOrderRequestV4.LimitOrder(symbol, amount, side, price, ordId);
+
+            // Act
+            var result = await _apiClient.PlaceOrderAsync(req);
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Equal(symbol, result.Data.Pair);
+            Assert.Equal(amount, result.Data.Quantity);
+            Assert.Equal(price, result.Data.Price);
+
+
+        }
+
+        [Fact, TestPriority(2)]
+        public async Task GetOrderAsyncTest()
+        {
+            // Arrange
+            var id = ordId;
+            // Act
+            var result = await _apiClient.GetOrderAsync(id);
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Equal(id, result.Data.Id);
+        }
+
+        [Fact, TestPriority(2)]
+        public async Task GetOrderTradesAsync()
+        {
+            // Act
+            var result = await _apiClient.GetOrderTradesAsync(ordId);
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+        }
+
+        [Fact, TestPriority(3)]
+        public async Task CancelOrderAsync()
+        {
+
+            // Act
+            var result = await _apiClient.CancelOrderAsync(ordId);
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.True(result.Data.Success);
+
+        }
+
+        [Fact]
+        public async Task GetBalancesAsync()
+        {
+
+            // Act
+            var result = await _apiClient.GetBalancesAsync();
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Contains(result.Data, c => c.Currency == "BTC");
+
         }
     }
 }
--- Tests/KunaV4ApiClientIntegrationTests.cs
+++ Tests/KunaV4ApiClientIntegrationTests.cs
@@ -1,20 +1,43 @@
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Kuna.Net;
 using Kuna.Net.Objects.V4;
+using Kuna.Net.Objects.V4.Requests;
 using Microsoft.Extensions.Logging;
 using Xunit;
+using CryptoExchange.Net.Authentication;
 
-namespace Tests
+namespace Kuna.Net.Tests
 {
+    [TestCaseOrderer(
+    ordererTypeName: "Kuna.Net.Tests.PriorityOrderer",
+    ordererAssemblyName: "Tests")]
     public class KunaV4ApiClientIntegrationTests
     {
         private readonly KunaV4ApiClient _apiClient;
-
+        private static readonly Guid ordId = Guid.NewGuid();
         public KunaV4ApiClientIntegrationTests()
         {
-            // Initialize your KunaV4ApiClient with appropriate configuration and credentials
-            // You may need to provide mock credentials for testing or use a test environment.
-            _apiClient = (KunaV4ApiClient)new KunaClient(new KunaClientOptions(){LogLevel = LogLevel.Trace}).ClientV4;
+            var config = new ConfigurationBuilder().AddJsonFile("keys.json", optional: true).Build();
+            var key = config["key"];
+            var secret = config["secret"];
+            var singleKey = config["sinle-api-key"];
+
+            KunaApiCredentials? cred = null;
+            if (!string.IsNullOrEmpty(singleKey))
+            {
+                cred = new KunaApiCredentials(singleKey);
+            }
+            if (!string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(secret))
+            {
+                cred = new KunaApiCredentials(key, secret);
+            }
+            _apiClient = (KunaV4ApiClient)new KunaClient(
+                new KunaClientOptions()
+                    {
+                        LogLevel = LogLevel.Trace,
+                        ApiCredentials = cred
+                    }).ClientV4;
         }
 
         // Add integration tests for each async method in your class
@@ -48,34 +71,187 @@ namespace Tests
             Assert.IsType<KunaTickerV4>(result.Data.First());
         }
         [Fact]
-        public async Task GetTickersAsyncForBTC_UAH_Test()
+        public async Task GetTickersAsyncForBTCUAH_BTCSUDT_Test()
         {
             // Arrange
-            var symbol = "BTC_UAH";
+            var symbol1 = "BTC_UAH";
+            var symbol2 = "BTC_USDT";
             // Act
-            var result = await _apiClient.GetTickersAsync(symbols: symbol);
+            var result = await _apiClient.GetTickersAsync(default, symbol1, symbol2);
 
             // Assert
             Assert.True(result.Success);
             // Add more assertions based on the expected behavior of this method
-            Assert.Equal(symbol, result.Data.First().Pair);
+            Assert.Contains(result.Data, t => t.Pair == symbol1);
+            Assert.Contains(result.Data, t => t.Pair == symbol2);
+        }
+        [Fact]
+        public async Task GetCurrenciesAsyncTest()
+        {
+            // Arrange
+
+            // Act
+            var result = await _apiClient.GetCurrenciesAsync();
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Contains(result.Data, c => c.Type == CurrencyType.Crypto);
+            Assert.Contains(result.Data, c => c.Type == CurrencyType.Fiat);
+
+        }
+        [Fact]
+        public async Task GetCurrenciesAsyncOnlyCryptoTest()
+        {
+            // Arrange
+
+            // Act
+            var result = await _apiClient.GetCurrenciesAsync(CurrencyType.Crypto);
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Contains(result.Data, c => c.Type == CurrencyType.Crypto);
+            Assert.DoesNotContain(result.Data, c => c.Type == CurrencyType.Fiat);
+
+        }
+        [Fact]
+        public async Task GetRecentPublicTradesAsyncTest()
+        {
+            // Arrange
+            var pair = "BTC_UAH";
+            var today = DateTime.Now.Date;
+            // Act
+            var result = await _apiClient.GetRecentPublicTradesAsync(pair);
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Contains(result.Data, c => c.Pair == pair);
+            Assert.Contains(result.Data, c => c.CreatedAt.Date == today);
+
         }
 
-        // Repeat the above pattern for each async method in your class
+        [Fact]
+        public async Task GetTradingPairsAsyncTest()
+        {
+            // Arrange
+            var pair = "BTC_UAH";
+            // Act
+            var result = await _apiClient.GetTradingPairsAsync();
 
-        // [Fact]
-        // public async Task PlaceOrderAsyncTest()
-        // {
-        //     // Arrange
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Contains(result.Data, c => c.Pair == pair);
+        }
 
-        //     // Act
-        //     var result = await _apiClient.PlaceOrderAsync(/* provide necessary parameters */);
+        [Fact]
+        public async Task GetOrderBookAsyncTest()
+        {
+            // Arrange
+            var pair = "BTC_UAH";
+            // Act
+            var result = await _apiClient.GetOrderBookAsync(pair, OrderBookLevel.Five);
 
-        //     // Assert
-        //     Assert.True(result.Success);
-        //     // Add more assertions based on the expected behavior of this method
-        // }
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.True(result.Data.Asks.First().Price > result.Data.Bids.First().Price);
+        }
 
-        // Create integration tests for other methods following the same pattern
+        [Fact]
+        public async Task SendAsProTest()
+        {
+            // Arrange
+            var pair = "BTC_UAH";
+
+            // Act
+            _apiClient.SetProAccount(true);
+            var result = await _apiClient.GetOrderBookAsync(pair, OrderBookLevel.Twenty);
+            _apiClient.SetProAccount(false);
+            var proHeaderEntry = result.RequestHeaders?.FirstOrDefault(kv => kv.Key == "account");
+            // Assert
+            Assert.True(result.Success);
+
+            // Add more assertions based on the expected behavior of this method
+            Assert.Contains(proHeaderEntry.Value.Value, p => p.Equals("pro"));
+        }
+
+        [Fact, TestPriority(1)]
+        public async Task PlaceOrderAsyncTest()
+        {
+            // Arrange
+            var symbol = "BTC_UAH";
+            var price = 10m;
+            var amount = 0.1m;
+            var side = KunaOrderSideV4.Bid;
+            var req = PlaceOrderRequestV4.LimitOrder(symbol, amount, side, price, ordId);
+
+            // Act
+            var result = await _apiClient.PlaceOrderAsync(req);
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Equal(symbol, result.Data.Pair);
+            Assert.Equal(amount, result.Data.Quantity);
+            Assert.Equal(price, result.Data.Price);
+
+
+        }
+
+        [Fact, TestPriority(2)]
+        public async Task GetOrderAsyncTest()
+        {
+            // Arrange
+            var id = ordId;
+            // Act
+            var result = await _apiClient.GetOrderAsync(id);
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Equal(id, result.Data.Id);
+        }
+
+        [Fact, TestPriority(2)]
+        public async Task GetOrderTradesAsync()
+        {
+            // Act
+            var result = await _apiClient.GetOrderTradesAsync(ordId);
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+        }
+
+        [Fact, TestPriority(3)]
+        public async Task CancelOrderAsync()
+        {
+            
+            // Act
+            var result = await _apiClient.CancelOrderAsync(ordId);
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.True(result.Data.Success);
+
+        }
+
+        [Fact]
+        public async Task GetBalancesAsync()
+        {
+            
+            // Act
+            var result = await _apiClient.GetBalancesAsync();
+
+            // Assert
+            Assert.True(result.Success);
+            // Add more assertions based on the expected behavior of this method
+            Assert.Contains(result.Data, c => c.Currency == "BTC");
+
+        }
     }
 }
